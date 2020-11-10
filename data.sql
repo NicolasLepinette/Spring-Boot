27a3a35e28408fc86c3807ec249b4ac59d892e6b
NicolasLepinette <55684429+NicolasLepinette@users.noreply.github.com>
2020-11-10 09:46:56 +0100

    Add files via upload

--- data.sql
+++ data.sql
@@ -1,4 +1,20 @@
 INSERT INTO Marque(id,nom)
 VALUES
-    (1,'Renault'),(2,'Ford'),(3,'Opel'),(4,'Peugeot'),(5,'Hyundai'),(6,'Honda'),(7,'Nissan');
+    (1,'Renault'),(2,'Ford'),(3,'Honda'),(4,'Peugeot'),(5,'Nissan');
 	
+INSERT INTO Concessionnaire(id,nom)
+VALUES
+    (1,'Garage Gambetta Seco'),(2,'Les autos de Jean-Marie'),(3,'Garage Paunet'),(4,'Otto Motot'),(5,'Garage Rage');
+	
+INSERT INTO Adresse(id,nom)
+VALUES
+    (1,'5 rue de la Paix Taissy'),(2,'10 rue de Sillery Taissy'),(3,'14 rue de l abbaye Beautheil-Saints '),(4,'20 ru des Sarrazins Lille'),(5,'19 rue du Sabot Lille');
+
+INSERT INTO Client(id,nom)
+VALUES
+    (1,'Bernard Cotteret'),(2,'Gilles de Retz'),(3,'Hugues de Vermandois'),(4,'Godefroy de Bouillon'),(5,'Bertrand du Guesclin'),(6,'Roland de Roncevaux'),(7,'Paul Reynauc'),(8,'Goerges Clemenceau'),(9,'Raymond Poincare'),(10,'Jean sans Peur');
+
+INSERT INTO Voiture(id,nom)
+VALUES
+    (1,'Clio'),(2,'Mondeo'),(3,'Talisman'),(4,'508'),(5,'Senic'),(6,'Primera'),(7,'Acor'),(8,'Focus'),(9,'206'),(10,'Micra');
+
